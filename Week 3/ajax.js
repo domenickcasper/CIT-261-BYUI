9e26f842b48b4453aee077321c45c12060db042b
Casper-Domenick <46412482+Casper-Domenick@users.noreply.github.com>
2019-03-14 17:43:57 -0600

    Fixed stuff

--- Week 3/ajax.js
+++ Week 3/ajax.js
@@ -43,7 +43,7 @@ function getJsonFromServer() {
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
-            document.getElementById("demoo").innerHTML = obj;
+            document.getElementById("demoo").innerHTML = JSON.stringify(obj);
         }
     };
     xmlhttp.open("GET", "domenick.txt", true);
